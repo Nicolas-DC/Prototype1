f81185ded0eb5c9a5f3d5e6f81577b924e26688b
Nicolas-DC <36486529+Nicolas-DC@users.noreply.github.com>
2019-10-04 09:17:53 -0400

    Initial commit

---
+++ Assets/Scripts/ThingToDeleteLater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingToDeleteLater : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move Backwards
+        transform.Translate(Vector3.back * Time.deltaTime* speed);
+    }
+}
